73d651fc180da398cce40263a8524fe20ea997e7
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2024-08-05 21:26:00 -0300

    chore: project reorganization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import AddTask from './components/AddTask.js';
+import AddUser from './components/AddUser.js';
+import TaskList from './components/TaskList.js';
+import { TasksProvider } from './hooks/TaskContext.js';
+import { UserProvider } from './hooks/UserContext.js';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserProvider>
+      <TasksProvider>
+        <h1>Tasks</h1>
+        <AddUser />
+        <h2>Day off in Kyoto</h2>
+        <AddTask />
+        <TaskList />
+      </TasksProvider>
+    </UserProvider>
   );
 }
-
-export default App;
